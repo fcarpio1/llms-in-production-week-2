8032ce29199b7ada3912f407f79a01310473c63b
fcarpio1 <fredrik.carpio@gmail.com>
2024-11-18 04:11:14 +0000

    fcarpio

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,15 @@
+# PROMPT = """
+# Generate a valid SQL query for the following natural language instruction:
+
+# Query: ${query}
+
+# ${gr.complete_json_suffix_v3}
+# """
+
 PROMPT = """
 Generate a valid SQL query for the following natural language instruction:
 
-Query: ${query}
+Here is the query: ${query}
 
 ${gr.complete_json_suffix_v3}
 """
